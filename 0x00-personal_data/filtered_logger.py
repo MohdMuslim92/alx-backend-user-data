a87e0c245ca8bd5735010fd87048f38d3d7d31cc
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-01-11 17:13:55 +0300

    Update get_db function
    - Added type hints to the get_db function, specifying the return type as
      mysql.connector.connection.MySQLConnection.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -41,7 +41,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Returns a MySQL database connection."""
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")

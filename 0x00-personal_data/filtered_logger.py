28ca85deab0e35faca6941dfc90fbb2d793ec1ed
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-01-11 14:26:00 +0300

    refactor: Improve code structure and readability in filtered_logger module
    - Replaced the dynamic construction of the regular expression in favor of
      a dictionary (`pattern`).
    - The dictionary includes two lambda functions: `extract` for pattern
      extraction and `replace` for substitution.
    - The `filter_datum` function now uses these patterns for better clarity.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,9 +7,15 @@ import re
 from typing import List
 
 
+pattern = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
     """Replaces specified fields in a log message with redaction."""
-    pattern = re.compile(f'({"|".join(fields)}=)[^{separator}]*')
-    return pattern.sub(fr'\1{redaction}', message)
+    extract, replace = (pattern["extract"], pattern["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)

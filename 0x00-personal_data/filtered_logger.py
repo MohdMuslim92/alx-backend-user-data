3239d11672195129b0964459d5c0b67f05743c77
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-01-11 14:15:45 +0300

    Reduced docstring for function filter_datum to match requirements

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,16 +15,6 @@ def filter_datum(
         ) -> str:
     """
     Replaces specified fields in a log message with redaction.
-
-    Args:
-        fields: a list of strings representing fields to obfuscate
-        redaction: a string representing by what the field will be obfuscated
-        message: a string representing the log line
-        separator: a string representing by which character separates all
-        fields in the log line
-
-    Returns:
-        The log message with specified fields obfuscated.
     """
     pattern = re.compile(f'({"|".join(fields)}=)[^{separator}]*')
     return pattern.sub(fr'\1{redaction}', message)

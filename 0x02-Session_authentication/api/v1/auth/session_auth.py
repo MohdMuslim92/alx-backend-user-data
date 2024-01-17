08c03f1b5f2588a09ac1671a0ed9cf2c79ab40e5
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-01-18 00:03:20 +0300

    Implement SessionAuth class
    - SessionAuth class, which inherits from Auth.
    - The SessionAuth class includes a class attribute `user_id_by_session_id`
      initialized as an empty dictionary and an instance method
      `create_session(self, user_id: str = None) -> str` for generating Session IDs.
    - The create_session method returns a Session ID if the provided user_id is
      a non-empty string. It utilizes the uuid module to generate unique
      Session IDs, associating each Session ID with a user_id in the
      `user_id_by_session_id` dictionary.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,24 @@
 SessionAuth module
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ SessionAuth class """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a Session ID for a user_id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        # Generate a Session ID using uuid module and uuid4()
+        session_id = str(uuid.uuid4())
+
+        # Use this Session ID as the key of the dictionary
+        # user_id_by_session_id, the value for this key must be user_id
+        self.user_id_by_session_id[session_id] = user_id
+
+        # Return the Session ID
+        return session_id

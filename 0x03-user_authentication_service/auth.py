54abe50b0aad82520e2a927d622e87b02e8cc399
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-01-25 01:52:01 +0300

    Implement Auth.destroy_session
    - The method takes a single user_id integer argument and returns None.
    - The method updates the corresponding user’s session ID to None.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,3 +73,13 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy the session for the user with the given user_id.
+        """
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+            user.session_id = None
+            self._db._session.commit()
+        except NoResultFound:
+            pass
